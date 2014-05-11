c5c63e06ee4d4de0ac814855fbb361ef584234bc
Philip Washington Sorst <philip@sorst.net>
2014-05-11 21:38:04 +0200

    Updating d3 header items

---
+++ src/main/java/net/dontdrinkandroot/wicket/headeritem/D3V33JsHeaderItem.java
@@ -0,0 +1,13 @@
+package net.dontdrinkandroot.wicket.headeritem;
+
+import org.apache.wicket.markup.head.JavaScriptUrlReferenceHeaderItem;
+
+
+public class D3V33JsHeaderItem extends JavaScriptUrlReferenceHeaderItem {
+
+	public D3V33JsHeaderItem() {
+
+		super("http://static.dontdrinkandroot.net/d3js/3.3/d3.min.js", "d3-3.3.js", false, null, null);
+	}
+
+}
---
+++ src/main/java/net/dontdrinkandroot/wicket/headeritem/D3V34JsHeaderItem.java
@@ -0,0 +1,13 @@
+package net.dontdrinkandroot.wicket.headeritem;
+
+import org.apache.wicket.markup.head.JavaScriptUrlReferenceHeaderItem;
+
+
+public class D3V34JsHeaderItem extends JavaScriptUrlReferenceHeaderItem {
+
+	public D3V34JsHeaderItem() {
+
+		super("http://static.dontdrinkandroot.net/d3js/3.4/d3.min.js", "d3-3.4.js", false, null, null);
+	}
+
+}
