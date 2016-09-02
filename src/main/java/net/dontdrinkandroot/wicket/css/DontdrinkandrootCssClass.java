aa0ee2a44069c69856458f20808d0e8dd0164c59
Philip Washington Sorst <philip@sorst.net>
2016-09-02 22:18:21 +0200

    Adding padding/margin classes

--- src/main/java/net/dontdrinkandroot/wicket/css/DontdrinkandrootCssClass.java
+++ src/main/java/net/dontdrinkandroot/wicket/css/DontdrinkandrootCssClass.java
@@ -3,7 +3,11 @@ package net.dontdrinkandroot.wicket.css;
 public enum DontdrinkandrootCssClass implements CssClass
 {
 	BTN_ICON("btn-icon"),
-	LIST_GROUP_PLAIN("list-group-plain");
+	LIST_GROUP_PLAIN("list-group-plain"),
+	MARGIN_TOP_NONE("margin-top-none"),
+	MARGIN_BOTTOM_NONE("margin-bottom-none"),
+	PADDING_TOP_NONE("padding-top-none"),
+	PADDING_BOTTOM_NONE("padding-bottom-none");
 
 	private String msgKey;
 
