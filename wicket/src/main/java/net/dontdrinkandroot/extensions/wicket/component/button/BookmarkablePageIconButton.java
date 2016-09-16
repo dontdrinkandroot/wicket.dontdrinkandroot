1e940e952e664fbb7781a2bad670fb127fb3bd0e
Philip Washington Sorst <philip@sorst.net>
2016-09-16 14:10:32 +0200

    Convenience constructors

--- wicket/src/main/java/net/dontdrinkandroot/extensions/wicket/component/button/BookmarkablePageIconButton.java
+++ wicket/src/main/java/net/dontdrinkandroot/extensions/wicket/component/button/BookmarkablePageIconButton.java
@@ -5,9 +5,11 @@ import net.dontdrinkandroot.wicket.behavior.CssClassAppender;
 import net.dontdrinkandroot.wicket.behavior.TitleModifier;
 import net.dontdrinkandroot.wicket.bootstrap.behavior.IconBehavior;
 import net.dontdrinkandroot.wicket.bootstrap.component.button.BookmarkablePageButton;
+import net.dontdrinkandroot.wicket.bootstrap.css.FontAwesomeIconClass;
 import net.dontdrinkandroot.wicket.css.CssClass;
 import org.apache.wicket.Page;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class BookmarkablePageIconButton<T> extends BookmarkablePageButton<T>
@@ -15,6 +17,19 @@ public class BookmarkablePageIconButton<T> extends BookmarkablePageButton<T>
     private IModel<CssClass> iconClassModel;
 
     private IModel<String> titleModel;
+
+    public <C extends Page> BookmarkablePageIconButton(
+            String id,
+            Class<C> pageClass,
+            FontAwesomeIconClass fontAwesomeIcon,
+            String title
+    )
+    {
+        super(id, pageClass);
+
+        this.iconClassModel = Model.of(fontAwesomeIcon.createIcon().setFixedWidth(true));
+        this.titleModel = Model.of(title);
+    }
 
     public <C extends Page> BookmarkablePageIconButton(
             String id,
