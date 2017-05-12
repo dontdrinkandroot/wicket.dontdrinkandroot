015d6d8debfa324d034b3398c792e9864aeef0a9
Philip Washington Sorst <philip@sorst.net>
2017-05-12 14:43:34 +0200

    Refactoring EntityTable

--- wicket/src/main/java/net/dontdrinkandroot/extensions/wicket/bootstrap/table/EntityTable.java
+++ wicket/src/main/java/net/dontdrinkandroot/extensions/wicket/bootstrap/table/EntityTable.java
@@ -2,17 +2,13 @@ package net.dontdrinkandroot.extensions.wicket.bootstrap.table;
 
 import net.dontdrinkandroot.wicket.behavior.CssClassAppender;
 import net.dontdrinkandroot.wicket.bootstrap.css.BootstrapCssClass;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.ajax.markup.html.repeater.data.sort.AjaxFallbackOrderByBorder;
-import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortStateLocator;
+import org.apache.wicket.extensions.ajax.markup.html.repeater.data.table.AjaxFallbackHeadersToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.HeadersToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.request.resource.PackageResourceReference;
 
 import javax.persistence.metamodel.SingularAttribute;
@@ -25,37 +21,14 @@ public class EntityTable<T> extends DataTable<T, SingularAttribute<T, ?>>
 {
     public EntityTable(
             String id,
-            List<? extends IColumn<T, SingularAttribute<T, ?>>> iColumns,
+            List<? extends IColumn<T, SingularAttribute<T, ?>>> columns,
             ISortableDataProvider<T, SingularAttribute<T, ?>> dataProvider
     )
     {
-        super(id, iColumns, dataProvider, 10);
-        this.setOutputMarkupId(true);
-        this.addTopToolbar(new HeadersToolbar<SingularAttribute<T, ?>>(this, dataProvider)
-        {
-            @Override
-            protected WebMarkupContainer newSortableHeader(
-                    String headerId,
-                    SingularAttribute<T, ?> property,
-                    ISortStateLocator<SingularAttribute<T, ?>> locator
-            )
-            {
-                return new AjaxFallbackOrderByBorder<SingularAttribute<T, ?>>(headerId, property, locator)
-                {
-                    @Override
-                    protected void onSortChanged()
-                    {
-                        EntityTable.this.setCurrentPage(0);
-                    }
+        super(id, columns, dataProvider, 10);
 
-                    @Override
-                    protected void onAjaxClick(AjaxRequestTarget target)
-                    {
-                        target.add(EntityTable.this);
-                    }
-                };
-            }
-        });
+        this.setOutputMarkupId(true);
+        this.addTopToolbar(new AjaxFallbackHeadersToolbar<>(this, dataProvider));
     }
 
     @Override
